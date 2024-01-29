7596f5eab8b8dcd62f7024f149059437fae2ed61
rushi <hrushikeshmysala2003@gmail.com>
2024-01-29 15:24:11 +0530

    Added functions to your raffle

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -8,25 +8,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
-    constructor(uint256 entranceFee) {
-        i_entranceFee=entranceFee;
+
+    event RaffleEnter(address indexed player);
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = entranceFee;
     }
+
     function enterRaffle() public payable {
         // msg.value > entrance fee
-        if(msg.value < i_entranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        // emit event after updating array or map
+        emit RaffleEnter(msg.sender);
     }
 
-    function getEntranceFee() {
+    function requestRandomWinner() external {
+        // Request the random number
+        // Once we get it, do
+        // 2 transaction process
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
 }
