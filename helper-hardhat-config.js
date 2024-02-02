5dbca72ae8e7926f666fcc18f985b343f34a6b63
rushi <hrushikeshmysala2003@gmail.com>
2024-02-02 10:09:48 +0530

    Deployed Mocks

---
+++ helper-hardhat-config.js
@@ -0,0 +1,13 @@
+const networkconfig = {
+    11155111: {
+        name: "sepolia",
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+    },
+}
+
+const devlopmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkconfig,
+    devlopmentChains,
+}
